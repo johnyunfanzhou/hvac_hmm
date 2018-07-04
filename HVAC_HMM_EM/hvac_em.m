06cd0eec734e4e00865faacfdc91523674bfd940
ZJC1013 <jiachenjason.zhou@mail.utoronto.ca>
2018-07-04 15:43:48 -0400

    update hvac_em.m, new sample_data_9.mat

--- HVAC_HMM_EM/hvac_em.m
+++ HVAC_HMM_EM/hvac_em.m
@@ -1,3 +1,5 @@
+clear;
+clc;
 O = 3*48*2*2;
 Q = 2;
 
@@ -9,7 +11,7 @@ Q = 2;
 % training data
 % T = 5;
 % nex = 10;
-data = importdata('sample_data_8.mat');
+data = importdata('sample_data_9.mat');
 
 % initial guess of parameters
 prior1 = normalise(rand(Q,1));
@@ -17,7 +19,7 @@ transmat1 = mk_stochastic(rand(Q,Q));
 obsmat1 = mk_stochastic(rand(Q,O));
 
 % improve guess of parameters using EM
-[LL, prior2, transmat2, obsmat2] = dhmm_em(data, prior1, transmat1, obsmat1, 'max_iter', 10);
+[LL, prior2, transmat2, obsmat2] = dhmm_em(data, prior1, transmat1, obsmat1, 'max_iter', 20);
 LL
 prior2
 transmat2
@@ -33,3 +35,12 @@ obslik = multinomial_prob(obs, obsmat2);
 
 [alpha, beta, gamma, loglik, xi, gamma2] = fwdback(prior2, transmat2, obslik, 'fwd_only', 1)
 alpha
+result_seq = zeros(1,size(alpha,2));
+for i = 1:size(result_seq,2)
+    if alpha(1,i) >= alpha(2,i)
+        result_seq(i) = 1;
+    else
+        result_seq(i) = 2;
+    end
+end
+plot(result_seq)

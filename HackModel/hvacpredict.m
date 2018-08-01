1c5e5e1ccad578f0e4715e66697fb9b56c255161
John Zhou <john_zhou@Johns-MacBook-Pro.local>
2018-08-01 16:58:19 -0400

    Added comments

--- HackModel/hvacpredict.m
+++ HackModel/hvacpredict.m
@@ -1,5 +1,27 @@
 function [new_data, forward_prob] = hvacpredict(A, B, data, Sn, Hn, Wn, narray, supressOutput, forward_prob, abortCheck)
-    
+% Inputs:
+%   A: transition matrix, 576x2
+%   B: Emission matrix, 576x2
+%   data: entire data read from CSV, matrix of size (# sample)x4, each row
+%         is (M, S, H, W).
+%   Sn: # of possible values for season (3)
+%   Hn: # of possible values for hour (48)
+%   Wn: # of possible values for week (2)
+%   narray: a list of indexes. Data at these indexes are used as test data,
+%           and prediction will be performed on these data.
+%   supressOutput: default to false
+%   forward_prob: probability matrix of size (# sample)x2. Entry at (k, 0)
+%                 represents the probability that M at that k's data is 0, 
+%                 and entry at (k, 1) represents the probability that M at 
+%                 that k's data is 1.
+%                 If the k's data is observed (in the training set), row k
+%                 of this matrix should be set as [0.0, 1.0] or [1.0, 0.0].
+%                 Default fills all training data rows as [0.0, 1.0] or 
+%                 [1.0, 0.0] according to data, and leaves testing data
+%                 rows as [0.0, 0.0] to be updated later during prediction.
+%   abortCheck: boolean if all input should be checked for correctness.
+%               Default at false;
+
     datasize = size(data, 1);
     nsize = size(narray, 2);
 
@@ -44,12 +66,16 @@ function [new_data, forward_prob] = hvacpredict(A, B, data, Sn, Hn, Wn, narray, 
     
     new_data = data;
 
+    % map (0 : 1, S, H, W) to one pair of numbers
     index = 2 * (h + Hn * (w + Wn * s)) + 1 : 2 * (h + Hn * (w + Wn * s)) + 2;
     
     clear abortCheck datacontinuous data prev_forward_prob;
     
     if n == 1
         warning('Predicting first entry. Previous forward probability is assumed [0.5, 0.5].');
+        % New observation probability for O_t+1 = 
+        % previous observation probability for O_t * P(S_t|O_t) *
+        % (PS_t+1|S_t) * P(O_t+1|S_t+1)
         forward_prob(n, :) = ([0.5, 0.5] * (B(index, :) ./ sum(B(index, :)))') * A(index, :) * B(index, :);
     else
         forward_prob(n, :) = (forward_prob(n - 1, :) * (B(index, :) ./ sum(B(index, :)))') * A(index, :) * B(index, :);
